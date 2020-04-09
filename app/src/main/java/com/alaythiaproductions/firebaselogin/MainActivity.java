3b483e0fc0247e950daf075f42b697cd032bb52b
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-09 12:06:23 -0500

    Add Registration and update color scheme

--- app/src/main/java/com/alaythiaproductions/firebaselogin/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/firebaselogin/MainActivity.java
@@ -1,14 +1,68 @@
 package com.alaythiaproductions.firebaselogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
+    private EditText registerEmail, registerPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerEmail = findViewById(R.id.register_email);
+        registerPassword = findViewById(R.id.register_password);
+
+        btnSignUp = findViewById(R.id.register_button);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        String email, password;
+        email = registerEmail.getText().toString().trim();
+        password = registerPassword.getText().toString().trim();
+
+        if (email.equals("")) {
+            Toast.makeText(MainActivity.this, "Email Required", Toast.LENGTH_SHORT).show();
+        } else if (password.equals("")) {
+            Toast.makeText(MainActivity.this, "Password Required", Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 6) {
+            Toast.makeText(MainActivity.this, "Password Must Be 6 Characters or More", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
